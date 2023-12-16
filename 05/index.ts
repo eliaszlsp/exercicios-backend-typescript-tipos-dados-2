8cfcc77de69edfe79a28434e243b9a33c8a5e4a3
eliaszlsp <estudoselias@hotmail.com>
2023-12-15 21:08:16 -0300

    end

--- 05/index.ts
+++ 05/index.ts
@@ -1,13 +1,15 @@
 import { Usuario } from "./types";
 import fs from "node:fs";
 
-const listarUsuarioPorProfissao = (profissao: string): Usuario[] | String => {
+const listarUsuarioPorProfissao = (profissao?: string): Usuario[] | String => {
   const file: Usuario[] = JSON.parse(fs.readFileSync("bd.json", "utf8"));
   const usuarioUnico = file.filter((usuario: Usuario) => {
-    return usuario.profissao === profissao;
+    if (profissao) return usuario.profissao === profissao;
+
+    return file;
   });
 
   return usuarioUnico.length > 0 ? usuarioUnico : "usuario não encontrado";
 };
 
-console.log(listarUsuarioPorProfissao("dev_full_stack"));
+console.log(listarUsuarioPorProfissao());
